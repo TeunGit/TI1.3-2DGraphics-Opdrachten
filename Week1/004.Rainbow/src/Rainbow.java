9f65cd5536a972cfd23b19c8c7212f95ddb4757b
TeunGit <afm.lauwerijssen@student.avans.nl>
2023-01-30 19:54:13 +0100

    Regenboog gemaakt

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -1,12 +1,14 @@
-import java.awt.*;
-import java.awt.geom.*;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
+
+import java.awt.*;
+import java.awt.geom.Line2D;
+
+import static javafx.application.Application.launch;
 
 public class Rainbow extends Application {
     @Override
@@ -20,7 +22,24 @@ public class Rainbow extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        graphics.translate(1920/2,1000);
+        graphics.scale(1,-1);
 
+        float resolution = (float)0.001;
+        float radiusBinnen = (float) 600;
+        float radiusBuiten = (float) 700;
+        System.out.println((float)500/+(float)Math.PI);
+        float increment = ((float)500.0/((float)Math.PI));
+        for(float i = 0; i < Math.PI; i+= resolution) {
+
+            graphics.setColor(Color.getHSBColor(i*increment/500.0f, 1, 1));
+            System.out.println(i*increment);
+            float x1 = radiusBinnen * (float)Math.cos(i);
+            float y1 = radiusBinnen * (float)Math.sin(i);
+            float x2 = radiusBuiten * (float)Math.cos(i);
+            float y2 = radiusBuiten * (float)Math.sin(i);
+            graphics.draw(new Line2D.Double(x1,y1,x2,y2));
+        }
     }
     
     
