5656810b2d5ae42135612ef4a3adb10a7fbdf9ed
TeunGit <afm.lauwerijssen@student.avans.nl>
2023-02-12 16:09:00 +0100

    made GradientPaintExercise

--- Week2/004.GradientPaint/src/GradientPaintExercise.java
+++ Week2/004.GradientPaint/src/GradientPaintExercise.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.MouseListener;
 import java.awt.geom.*;
 
 import javafx.application.Application;
@@ -23,8 +24,9 @@ public class GradientPaintExercise extends Application {
         mainPane.setCenter(canvas);
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("GradientPaint");
+        canvas.setOnMouseDragged(mouseEvent -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         primaryStage.show();
-        draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+
     }
 
 
@@ -33,6 +35,12 @@ public class GradientPaintExercise extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        Area rectangle = new Area(new Rectangle2D.Double(0,0, canvas.getWidth(),canvas.getHeight()));
+        float[] fractions = {(float)0.1,(float) 0.2,(float)0.3,(float)0.4,(float) 0.5,(float) 0.6,(float)0.7,(float) 0.8,(float)0.9,1};
+        Color[] colors = {Color.BLACK, Color.BLUE, Color.CYAN, Color.GRAY, Color.GREEN, Color.magenta, Color.ORANGE, Color.PINK, Color.RED,Color.YELLOW};
+        graphics.setPaint(new RadialGradientPaint(new Point2D.Double(canvas.getWidth()/2,canvas.getHeight()/2),500, MouseInfo.getPointerInfo().getLocation(), fractions,colors,MultipleGradientPaint.CycleMethod.REFLECT));
+        graphics.fill(rectangle);
+        graphics.draw(rectangle);
     }
 
 
