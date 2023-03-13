f7e42ad9d174daba163ca62a889225fd9303aeea
TeunGit <afm.lauwerijssen@student.avans.nl>
2023-03-13 20:26:32 +0100

    made spotlight

--- Week3/004.Spotlight/src/Spotlight.java
+++ Week3/004.Spotlight/src/Spotlight.java
@@ -1,7 +1,9 @@
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -10,6 +12,7 @@ import static javafx.application.Application.launch;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -20,15 +23,25 @@ import org.jfree.fx.ResizableCanvas;
 
 public class Spotlight extends Application {
     private ResizableCanvas canvas;
-
+    private double mouseX;
+    private double mouseY;
+    private BufferedImage image;
     @Override
     public void start(Stage stage) throws Exception
     {
-
+        try {
+            image = ImageIO.read(getClass().getResource("pepe-the-frog.jpg"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+        mouseX = canvas.getWidth()/2;
+        mouseY = canvas.getHeight()/2;
+        canvas.setOnMouseMoved(e -> mouseMoved(e));
+        canvas.setOnMouseClicked(e -> mouseClicked(e));
         new AnimationTimer() {
             long last = -1;
 
@@ -49,18 +62,40 @@ public class Spotlight extends Application {
         draw(g2d);
     }
 
+    private void mouseMoved(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+    private boolean imageAsBackground = false;
+    private void mouseClicked(MouseEvent e){
+        imageAsBackground = !imageAsBackground;
+    }
 
-    public void draw(FXGraphics2D graphics)
-    {
+
+
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
+        graphics.setBackground(Color.black);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        Shape spotlightShape = new Ellipse2D.Double(mouseX - 100, mouseY - 100, 200, 200);
+
+        graphics.draw(spotlightShape);
+        graphics.setClip(spotlightShape);
+        if (imageAsBackground) {
+            AffineTransform tx = new AffineTransform();
+            tx.scale(canvas.getWidth()/image.getWidth(),canvas.getHeight()/image.getHeight());
+
+            graphics.drawImage(image, tx, null);
+        } else {
+            for (int i = 0; i < 100; i++) {
+                graphics.setPaint(Color.getHSBColor((float) (500f * Math.random()), 1, 1));
+                graphics.drawLine((int) (canvas.getWidth() * Math.random()), (int) (canvas.getHeight() * Math.random()), (int) (canvas.getWidth() * Math.random()), (int) (canvas.getHeight() * Math.random()));
+            }
+        }
+        graphics.setClip(null);
     }
 
-    public void init()
-    {
 
-    }
 
     public void update(double deltaTime)
     {
